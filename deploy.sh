2df0e0743d9c423497ca34de958255ad9e5161fb
Yamel Senih <ysenih@erpya.com>
2021-02-16 14:34:31 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f "https://${access_token}@github.com/erpcya/adempiere-vue-site.git" master:gh-pages
+git push -f "https://${access_token}@github.com/adempiere/adempiere-vue-site.git" master:gh-pages
 
 cd -
